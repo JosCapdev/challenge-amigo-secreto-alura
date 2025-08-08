7ad90cefdae61bda2cb32910a5a9986741d47bfb
JosCapdev <josecapdevila512@gmail.com>
2025-08-08 13:59:12 -0300

    Implementa app Amigo Secreto con funciones para agregar amigos, validar nombres, sortear aleatoriamente y agrega README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1>Challenge Amigo Secreto</h1>
+
+# Amigo Secreto
+
+Una aplicación web sencilla para organizar sorteos de 'Amigo Secreto' entre amigos o familiares.
+
+## Funcionalidades
+
+*   Permite agregar nombres a una lista.
+*   Verifica que no se ingresen nombres duplicados.
+*   Muestra la lista de participantes.
+*   Realiza el sorteo de forma aleatoria.
+*   Muestra el resultado del sorteo.
+
+## Cómo utilizarlo
+
+1.  Ingresa el nombre de un amigo en el campo de texto.
+2.  Haz clic en el botón 'Añadir' para agregar el nombre a la lista.
+3.  Repite los pasos 1 y 2 para agregar todos los participantes.
+4.  Haz clic en el botón 'Sortear amigo' para realizar el sorteo.
+5.  El nombre del amigo sorteado se mostrará en la sección de resultados.
+
+## Tecnologías utilizadas
+
+*   HTML
+*   CSS
+*   JavaScript
+
+## Autor
+
+Git: JosCapdev
--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+}
+
+function limpiarCampo(){
+    document.querySelector('#amigo').value = '';
+}
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo !== ''){
+        if(amigos.includes(amigo)){
+            asignarTextoElemento('h2','El nombre ya fue ingresado,intente ingresar otro :)');
+            limpiarCampo();
+        }else{
+            amigos.push(amigo);
+            limpiarCampo();
+            condicionesIniciales();
+            actualizarLista();
+        }
+    }else{
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function limpiarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+}
+
+function agregarAmigoALista(amigo) {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML += `<li>${amigo}</li>`;
+}
+
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    limpiarLista();
+    for (let amigo of amigos) {
+         agregarAmigoALista(amigo);
+    }
+}
+
+function sortearAmigo(){
+    condicionesIniciales();
+    if(amigos.length === 0){
+        asignarTextoElemento('h2', 'No hay amigos en la lista. Por favor, agrega algunos amigos antes de sortear.');
+    }else{
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        asignarTextoElemento('#resultado', 'El amigo sorteado es: ' + amigoSorteado);
+    }
+}

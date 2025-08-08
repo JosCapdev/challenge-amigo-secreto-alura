24ce6f95d37aeefaa06eaf3516d4357177658343
JosCapdev <josecapdevila512@gmail.com>
2025-08-08 18:36:29 -0300

    agregado de nuevas funcionalidades en app.js y un boton en index.html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let amigos = []
+let amigos = [];
+let amigosYaSorteados = [];
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -8,6 +9,7 @@ function asignarTextoElemento(elemento, texto) {
 
 function condicionesIniciales() {
     asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+    document.querySelector('#reiniciar').setAttribute('disabled', true);
 }
 
 function limpiarCampo(){
@@ -34,11 +36,12 @@ function agregarAmigo(){
 function limpiarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
+    asignarTextoElemento('#resultado','')
 }
 
 function agregarAmigoALista(amigo) {
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML += `<li>${amigo}</li>`;
+    lista.innerHTML += `<li> - ${amigo}</li>`;
 }
 
 
@@ -52,11 +55,44 @@ function actualizarLista() {
 
 function sortearAmigo(){
     condicionesIniciales();
+    bloqueoDeBotonesXComienzoSorteo();
     if(amigos.length === 0){
         asignarTextoElemento('h2', 'No hay amigos en la lista. Por favor, agrega algunos amigos antes de sortear.');
-    }else{
+    }else if(amigos.length < 3){
+        asignarTextoElemento('h2', 'Se necesitan al menos 3 amigos para realizar el sorteo.')
+    }else if(amigos.length !== amigosYaSorteados.length){
         let indiceAleatorio = Math.floor(Math.random() * amigos.length);
         let amigoSorteado = amigos[indiceAleatorio];
-        asignarTextoElemento('#resultado', 'El amigo sorteado es: ' + amigoSorteado);
+        
+        if(amigosYaSorteados.includes(amigoSorteado)){
+           return sortearAmigo();
+        }else{
+            amigosYaSorteados.push(amigoSorteado);
+            asignarTextoElemento('#resultado', 'El amigo sorteado es: ' + amigoSorteado);
+        }
+    }else{
+        asignarTextoElemento('h2', 'Ya se sortearon todos los Amigos!');
+        document.getElementById('sortear').setAttribute('disabled', true);
+        document.getElementById('reiniciar').removeAttribute('disabled', true);
     }
+}
+
+function bloqueoDeBotonesXComienzoSorteo(){
+    document.getElementById('amigo').setAttribute('disabled', true);
+    document.getElementById('agregar').setAttribute('disabled', true);
+}
+
+function DesbloquebloqueoDeBotones(){
+    document.getElementById('amigo').removeAttribute('disabled', true);
+    document.getElementById('agregar').removeAttribute('disabled', true);
+}
+
+function reiniciarJuego() {
+    amigos = [];
+    amigosYaSorteados = [];
+    limpiarCampo();
+    limpiarLista();
+    condicionesIniciales();
+    DesbloquebloqueoDeBotones();
+    document.getElementById('sortear').removeAttribute('disabled', true); 
 }
